20e3cea082233abf3c7d7df4ffdf1f4e769b4c98
MiguelARam27 <garandster27@live.com>
2021-02-03 22:31:09 -0500

    js for slider

--- restaurant-app/resources/views/includes/food-categories-slider.blade.php
+++ restaurant-app/resources/views/includes/food-categories-slider.blade.php
@@ -11,21 +11,21 @@
             <div class="sliding-system">
 
                 @foreach ($categories as $category)
-                <a href="/menu/{{$category->title}}">
-                    <div class="slide">
-                        <div class="background"></div>
-                        <div class="content">
-                            <div class="food-title">
-                                {{$category->title}}
-                            </div>
-                            <p class="food-description">
-                                {{$category->description}}
-                            </p>
-                            <div class="food-image">
-                                <img src="{{$category->image_url}}">
-                            </div>
+                <a href="/menu/{{$category->title}}" class='slide'>
+
+                    <div class="background"></div>
+                    <div class="content">
+                        <div class="food-title">
+                            {{$category->title}}
+                        </div>
+                        <p class="food-description">
+                            {{$category->description}}
+                        </p>
+                        <div class="food-image">
+                            <img src="{{$category->image_url}}">
                         </div>
                     </div>
+
                 </a>
                 @endforeach
 
@@ -34,3 +34,67 @@
         </div>
     </div>
 </section>
+<script src="/assets/vendor/jquery/jquery-3.3.1.min.js"></script>
+<script>
+    let slides = $('.sliding-system a.slide').length;
+    console.log(slides)
+    let maxSlides;
+    let moveTotal;
+    let currentSlide = 0;
+    $(window).resize(function(){
+      currentSlide = 0;
+      $('.sliding-system').css('transform', 'translate3d(-0%, 0,0');
+    })
+    function desktopSlider(){
+      if(a.matches){
+        maxSlides = Math.ceil((slides * 20) / 100);
+        moveTotal = 100;
+        // Right Button
+        $('.right-btn').on('click', () => {
+          console.log('clicked right btn')
+          if(currentSlide < maxSlides){
+            currentSlide += 100;
+            $('.sliding-system').css('transform', `translate3d(-${currentSlide}%, 0, 0)`);
+          }
+        });
+        // Left Button
+        $('.left-btn').on('click', () => {
+          console.log('clicked right btn')
+          if(currentSlide != 0){
+            currentSlide -= 100;
+            $('.sliding-system').css('transform', `translate3d(-${currentSlide}%, 0, 0)`);
+          }
+        });
+      }
+    }
+    var a = window.matchMedia("(min-width: 998px)")
+    desktopSlider(a);
+    a.addListener(desktopSlider);
+    function tabletSlider(){
+      if(b.matches){
+        maxSlides = Math.ceil((slides * 50) / 100);
+        moveTotal = 100;
+        // Right Button
+        $('.right-btn').on('click', () => {
+          
+          if(currentSlide < ((maxSlides * 100) - 100)){
+            currentSlide += 50;
+            $('.sliding-system').css('transform', `translate3d(-${currentSlide}%, 0, 0)`);
+          }
+        });
+        // Left Button
+        $('.left-btn').on('click', () => {
+       
+          if(currentSlide != 0){
+            currentSlide -= 50;
+            $('.sliding-system').css('transform', `translate3d(-${currentSlide}%, 0, 0)`);
+          }
+        });
+      }
+    }
+    var b = window.matchMedia("(min-width: 768px) and (max-width: 997px");
+    console.log(b)
+    tabletSlider(b);
+    b.addListener(tabletSlider);
+
+</script>
