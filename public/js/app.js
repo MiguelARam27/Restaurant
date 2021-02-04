8ae424c8461ffc8279a11b1a776330c1f03e912e
MiguelARam27 <garandster27@live.com>
2021-02-04 01:09:43 -0500

    Fixed errors for deployment

--- public/js/app.js
+++ public/js/app.js
@@ -37364,9 +37364,9 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/miguelramirez/Desktop/Projects/Restaurant/restaurant-app/resources/js/app.js */"./resources/js/app.js");
-__webpack_require__(/*! /Users/miguelramirez/Desktop/Projects/Restaurant/restaurant-app/resources/sass/app.scss */"./resources/sass/app.scss");
-module.exports = __webpack_require__(/*! /Users/miguelramirez/Desktop/Projects/Restaurant/restaurant-app/resources/sass/footer.scss */"./resources/sass/footer.scss");
+__webpack_require__(/*! /Users/miguelramirez/Desktop/Projects/Restaurant/resources/js/app.js */"./resources/js/app.js");
+__webpack_require__(/*! /Users/miguelramirez/Desktop/Projects/Restaurant/resources/sass/app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! /Users/miguelramirez/Desktop/Projects/Restaurant/resources/sass/footer.scss */"./resources/sass/footer.scss");
 
 
 /***/ })
